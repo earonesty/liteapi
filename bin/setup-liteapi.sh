e24113d774fed8ab302fddff429be4c452478203
Erik Aronesty <erik@q32.com>
2014-01-15 01:01:25 +0000

    more install help

--- bin/setup-liteapi.sh
+++ bin/setup-liteapi.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 if [ -z "$1" ]; then
     echo "usage $0 <host>"
@@ -10,9 +10,13 @@ HOME=/var/www/liteapi
 mkdir $HOME
 cd $HOME
 
-# this instead of git clone, to include settings, etc.
-rsync -a --exclude "**/coin/**" --exclude "**/log/**" $host:$HOME/ $HOME/ &
+git clone git@github.com:earonesty/liteapi.git
 
+# this instead of git clone, to include settings, etc.
+rsync -a $host:$HOME/src/ $HOME/src/ &
+rsync -a $host:$HOME/etc/ $HOME/etc/ &
+
+# get it all up-2-date
 yum -y update
 
 yum -y install bind-utils cpan
@@ -22,23 +26,28 @@ perl -MJSON::RPC::Client -e 1 2> /dev/null || cpan -i 'JSON::RPC::Client'
 perl -MURI::Encode -e 1 2> /dev/null || cpan -i 'URI::Encode'
 perl -MData::URIEncode -e 1 2> /dev/null || cpan -i 'Data::URIEncode'
 
-(crontab -l ; echo "* * * * * $HOME/bin/minutely")  |uniq - | crontab -
-(crontab -l ; echo "0 * * * * /var/www/liteapi/bin/hourly")  |uniq - | crontab -
-
 if [ ! -n "`which pgsql`" ]; then
     yum -y install postgresql postgresql-server postgresql-contrib 
     service postgresql initdb 
     rsync -a $host:/var/lib/pgsql/data/pg_hba.conf /var/lib/pgsql/data/pg_hba.conf
     /etc/init.d/postgresql start
+
+    # nicer when they're all the same number
     groupadd -g 502 lite
     useradd -u 502 -g 502 lite
+
+    # load user info
     sudo -u postgres psql < bin/user.sql
+    # load db itself
     sudo -u postgres psql lite < bin/lite.sql
 fi
 
-
+# this should work by now, if not, bash-e will drop out
 bin/litedb "select * from cbs"
 
+wait
+
+# as long as this is centos-6, don't bother recompiling
 cd src/openssl-1.0.1f
 make install
 cd $HOME
@@ -66,9 +75,17 @@ rm -rf /etc/maradns
 ln -fs $HOME/etc/maradns /etc/maradns
 ln -fs $HOME/etc/mararc /etc/mararc
 
+# replace ip in config files
 perl -i -pe "s{\\d+\\.\\d+\\.\\d+\\.\\d+}{$primaryip}" etc/mararc
 perl -i -pe "s{\\d+\\.\\d+\\.\\d+\\.\\d+}{$primaryip}" etc/maradns/db.liteapi.org
+perl -i -pe "s{\\d+\\.\\d+\\.\\d+\\.\\d+}{$primaryip}" etc/liteapi.conf
 
+# convenience
 ln -fs $HOME/bin/litessh /usr/local/bin/
 ln -fs $HOME/bin/litesync /usr/local/bin/
 ln -fs $HOME/bin/litedb /usr/local/bin/
+
+# crontab
+(crontab -l ; echo "* * * * * $HOME/bin/minutely")  |uniq - | crontab -
+(crontab -l ; echo "0 * * * * /var/www/liteapi/bin/hourly")  |uniq - | crontab -
+
